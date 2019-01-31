a6220aff825743b1c16df7a718ea1d8cfe0950da
Richie <richardokoye2@gmail.com>
2019-01-31 04:04:12 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MessageComponent } from './message/message.component';
+import { ProductEditComponent } from './products/product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MessageComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule
